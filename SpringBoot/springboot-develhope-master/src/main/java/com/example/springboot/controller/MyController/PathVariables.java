1ade323bf968bb09211b075d51cfc343d67ad682
Mario Tolvo <mariobros9@hotmail.it>
2023-05-17 17:16:52 +0200

    Secondo punto dell'esercizio

--- SpringBoot/springboot-develhope-master/src/main/java/com/example/springboot/controller/MyController/PathVariables.java
+++ SpringBoot/springboot-develhope-master/src/main/java/com/example/springboot/controller/MyController/PathVariables.java
@@ -3,6 +3,7 @@ package com.example.springboot.controller.MyController;
 import com.example.springboot.controller.Meal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -17,7 +18,17 @@ public class PathVariables {
     );
 
     @GetMapping(value = "/get/mario-meals")
-    public ResponseEntity<?> gregExercise(){
+    public ResponseEntity<List<Meal>> gregExercise(){
         return ResponseEntity.ok(marioMeals);
+    }
+
+    @GetMapping(value = "/get/mario-meals/{name}")
+    public ResponseEntity<Meal> getMealByName(@PathVariable("name") String name){
+        for (Meal meal : marioMeals) {
+            if (meal.getName().equalsIgnoreCase(name)) {
+                return ResponseEntity.ok(meal);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 }
